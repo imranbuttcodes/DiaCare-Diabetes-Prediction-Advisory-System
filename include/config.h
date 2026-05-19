cf0a9b9245bdc1880e8d496e22d295217154a781
MUhammad Imran Butt <imrancodes0546@gmail.com>
2026-05-19 15:02:45 +0500

    refactor: define risk thresholds

--- include/config.h
+++ include/config.h
@@ -55,3 +55,12 @@ const int EPOCHS = 5000;
 const double LEARNING_RATE = 0.1;
 const double THRESHOLD = 0.46;
 const double LAMBDA = 0.04; // for l2 regularization
+
+
+// Risk Thresholds 
+const double RISK_LOW = 0.20;
+const double RISK_MODERATE_LOW = 0.40;
+const double RISK_MODERATE = 0.60;
+const double RISK_HIGH = 0.80;
+
+const int WARD_MAX_CAPACITY = 10;
